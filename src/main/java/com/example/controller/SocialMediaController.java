7a28a270794f596a2a1c9517d8b4a5483f8f433b
Elijah Monzon <eli.develops@gmail.com>
2025-06-26 22:53:44 +0000

    controller can now process user registrations

--- src/main/java/com/example/controller/SocialMediaController.java
+++ src/main/java/com/example/controller/SocialMediaController.java
@@ -1,5 +1,18 @@
 package com.example.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.entity.Account;
+import com.example.entity.Message;
+import com.example.service.AccountService;
+import com.example.service.MessageService;
 
 /**
  * TODO: You will need to write your own endpoints and handlers for your controller using Spring. The endpoints you will need can be
@@ -7,6 +20,24 @@ package com.example.controller;
  * where applicable as well as the @ResponseBody and @PathVariable annotations. You should
  * refer to prior mini-project labs and lecture materials for guidance on how a controller may be built.
  */
+@RestController
 public class SocialMediaController {
 
+    @Autowired AccountService as;
+    @Autowired MessageService ms;
+
+    @PostMapping(value = "register")
+    public ResponseEntity register(@RequestBody Account acc){
+        boolean nameExists = as.verifyAccount(acc.getUsername()) != null; //username exists
+        boolean notValid = acc.getUsername().isBlank() || acc.getPassword().length() < 4; //username is blank or password less than 4
+        
+        if(nameExists) { 
+            return ResponseEntity.status(HttpStatus.CONFLICT).build();
+        } else if(notValid){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        } else{ //account successfully was created in db
+            Account savedAcc = as.createAccount(new Account(acc.getUsername(), acc.getPassword()));
+            return ResponseEntity.status(HttpStatus.OK).body(savedAcc);
+        }
+    }
 }
--- src/main/java/com/example/service/AccountService.java
+++ src/main/java/com/example/service/AccountService.java
@@ -1,6 +1,7 @@
 package com.example.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.*;
 import org.springframework.stereotype.Service;
 
 import com.example.entity.Account;

dc1481e429110440f425999d7bee79e6ff39c2df
Elijah Monzon <eli.develops@gmail.com>
2025-06-26 20:00:45 +0000

    set up jpa repos and setup for service classes

--- src/main/java/com/example/service/AccountService.java
+++ src/main/java/com/example/service/AccountService.java
@@ -1,4 +1,17 @@
 package com.example.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.example.repository.AccountRepository;
+
 public class AccountService {
+
+    AccountRepository repo;
+
+    @Autowired
+    public AccountService(AccountRepository repository){
+        this.repo = repository;
+    }
+    
+
 }
--- src/main/java/com/example/service/MessageService.java
+++ src/main/java/com/example/service/MessageService.java
@@ -1,4 +1,15 @@
 package com.example.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.example.repository.MessageRepository;
+
 public class MessageService {
+
+    MessageRepository repo;
+
+    @Autowired
+    public MessageService(MessageRepository repository){
+        this.repo = repository;
+    }
 }
